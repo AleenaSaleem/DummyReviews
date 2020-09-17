018fd0f006ba039852918c8d10eccd7c74354dbe
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-17 18:57:48 +0530

    Update ControllerTests.cs

--- ReceiverTests/ControllerTests.cs
+++ ReceiverTests/ControllerTests.cs
@@ -1,6 +1,9 @@
 
+
 using Xunit;
 using Receiver;
+using System.Collections.Generic;
+
 namespace ReceiverTests
 {
     public class ControllerTests
@@ -16,6 +19,30 @@ namespace ReceiverTests
             Assert.Equal(csvOutput, controller.OutputInterface);
 
         }
-
+        [Fact]
+        public void TestExpectingAppropriateReadInputMethodToBeCalled()
+        {
+            MockConsoleInput mockInput = new MockConsoleInput();
+            string filepath = "same_random_path";
+            MockCSVOutput mockOutput = new MockCSVOutput(filepath);
+            Controller controller = new Controller(mockInput, mockOutput);
+            var output = (List<List<string>>)controller.ReadInput();
+            Assert.Equal("sample00", output[0][0]);
+        }
+        [Fact]
+        public void TestExpectingAppropriateWriteOutputMethodToBeCalledWhenCalledWithValidIDictionary()
+        {
+            MockConsoleInput mockInput = new MockConsoleInput();
+            string filepath = "same_random_path";
+            MockCSVOutput mockOutput = new MockCSVOutput(filepath);
+            Controller controller = new Controller(mockInput, mockOutput);
+            IDictionary<string, int> dict = new Dictionary<string, int>();
+            dict.Add("sample1", 1);
+            dict.Add("sample2", 2);
+            controller.WriteOutput(dict);
+            Assert.True(mockOutput.OutputStatus);
+            Assert.Equal("sample1,1", mockOutput.MockFileOutput[0]);
+        }
     }
 }
+
