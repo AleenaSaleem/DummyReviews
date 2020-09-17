d92fe96b310b8bc2f77ab8c2e7fd7245dcd39b14
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-17 17:27:47 +0530

    Update ReceiverInputTests.cs

--- ReceiverTests/ReceiverInputTests.cs
+++ ReceiverTests/ReceiverInputTests.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Receiver;
+using ReceiverInput;
 using Xunit;
 namespace ReceiverTests
 {
@@ -13,17 +9,17 @@ namespace ReceiverTests
     {
         public IEnumerable<IEnumerable<string>> ReadInput()
         {
-            int n_rows, n_cols;
-            n_rows = 3;
-            n_cols = 3;
+            int nRows, nColumns;
+            nRows = 3;
+            nColumns = 3;
             var InputFromSender = new List<List<string>>();
-            for (int i = 0; i < n_rows; i++)
+            for (int i = 0; i < nRows; i++)
             {
                 var new_row = new List<string>();
-                for (int j = 0; j < n_cols; j++)
+                for (int j = 0; j < nColumns; j++)
                 {
                     new_row.Add("sample" + i.ToString() + j.ToString());
-
+                    
                 }
                 InputFromSender.Add(new_row);
             }
@@ -37,16 +33,17 @@ namespace ReceiverTests
             }
         }
     }
-
-    public class ReceiverInputTests
-    {
-        [Fact]
-        public void TestExpectingAnIEnumerableWhenCalledWithNumRowsAndNumColsAndData()
+        
+        public class ReceiverInputTests
         {
-
-            MockConsoleInput mockObject = new MockConsoleInput();
-            var ActualTestOutput = (List<List<string>>)mockObject.ReadInput();
-            Assert.Equal("sample00", ActualTestOutput[0][0]);
+            [Fact]
+            public void TestExpectingAnIEnumerableToBeReturnedWhenCalledWithNumRowsAndNumColsAndData()
+            {
+                
+                MockConsoleInput mockObject = new MockConsoleInput();
+                var ActualTestOutput = (List<List<string>>)mockObject.ReadInput();
+                Assert.Equal("sample00",ActualTestOutput[0][0]);
+            }
         }
-    }
 }
+
