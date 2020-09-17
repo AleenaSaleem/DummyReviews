b37fcbcffe2ba89374e2cad2de11b8d0a015f871
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-17 21:22:59 +0530

    Update Controller.cs

--- Sender/Controller.cs
+++ Sender/Controller.cs
@@ -23,7 +23,8 @@ namespace Sender
         }
         static void Main()
         {
-            const string filepath = @"D:\a\DummyReviews\DummyReviews\Sender\Comments.csv";
+            //const string filepath = @"D:\a\DummyReviews\DummyReviews\Sender\Comments.csv";
+            const string filepath = Console.ReadLine();
             var csvInput = new CsvInput(filepath);
             var consoleOutput = new ConsoleOutput();
             var controller = new Controller(csvInput, consoleOutput);
