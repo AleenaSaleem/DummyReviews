2fd9989cd1d8b36ad16a67a1601368b5756f5c64
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-17 16:44:24 +0530

    Update SenderOutputTests.cs

--- SenderTests/SenderOutputTests.cs
+++ SenderTests/SenderOutputTests.cs
@@ -23,7 +23,7 @@ namespace SenderTests
 
     public class SenderOutputTests
     {
-        readonly List<List<string>> testInput = new List<List<string>>
+         readonly List<List<String>> testInput = new List<List<string>>
             {
                 new List<string> { "Date", "Comment" },
                 new List<string> { "12/12/2012", "All good" },
