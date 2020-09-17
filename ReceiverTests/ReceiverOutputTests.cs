a0e9f4e767fca39a3d4885d192af4669a6661901
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-17 17:29:22 +0530

    Update ReceiverOutputTests.cs

--- ReceiverTests/ReceiverOutputTests.cs
+++ ReceiverTests/ReceiverOutputTests.cs
@@ -1,12 +1,50 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using Xunit;
+using Receiver;
 namespace ReceiverTests
 {
-    class ReceiverOutputTests
+    public class MockCSVOutput : IReceiverOutput
     {
+        string filepath;
+        public List<string> MockFileOutput = new List<string>();
+        public bool OutputStatus;
+        public MockCSVOutput(string filepath)
+        {
+            this.filepath = filepath;
+            this.OutputStatus = false;
+        }
+        public void WriteOutput(IDictionary<string, int> WordFrequency)
+        {
+            foreach (var line in WordFrequency)
+            {
+                var newLine = string.Format("{0},{1}", line.Key, line.Value);
+                MockFileOutput.Add(newLine);
+                
+            }
+            this.OutputStatus = true;
+        }
+    }
+    public class ReceiverOutputTests
+    {
+        [Fact]
+        public void TestExpectingStatusOfFileWrittenAsTrueWhenCalledWithDictionaryOfWordFrequnecy()
+        {
+            MockCSVOutput mockOutput = new MockCSVOutput("Random_file_path");
+            IDictionary<string, int> dict = new Dictionary<string, int>();
+            dict.Add("sample1", 1);
+            dict.Add("sample2", 2);
+            mockOutput.WriteOutput(dict);
+            Assert.True(mockOutput.OutputStatus);
+        }
+        [Fact]
+        public void TestExpectingValidMockFileOutputWhenCalledWithDictionaryOfWordFrequnecy()
+        {
+            MockCSVOutput mockOutput = new MockCSVOutput("Random_file_path");
+            IDictionary<string, int> dict = new Dictionary<string, int>();
+            dict.Add("sample1", 1);
+            dict.Add("sample2", 2);
+            mockOutput.WriteOutput(dict);
+            Assert.Equal("sample1,1",mockOutput.MockFileOutput[0]);
+        }
     }
 }
