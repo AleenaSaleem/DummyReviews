f36c69f9b9a8176de3e979985b3c2aa6bc3759c5
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-18 23:09:04 +0530

    Update ReceiverInput.cs

--- Receiver/ReceiverInput.cs
+++ Receiver/ReceiverInput.cs
@@ -1,8 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Diagnostics.CodeAnalysis;
+
 namespace Receiver
 {
     public interface IReceiverInput
@@ -14,40 +13,41 @@ namespace Receiver
     {
         public virtual IEnumerable<IEnumerable<string>> ReadInput()
         {
-            var nRows = ReadNumberOfRows();
-            var nCols = ReadNumberOfColumns();
+            int nRows = ReadNumberOfRows();
+            int nColumns = ReadNumberOfColumns();
+            
             var inputFromSender = new List<List<string>>();
-            for (var i = 0; i < nRows; i++)
+            for (int i = 0; i < nRows; i++)
             {
                 var newRow = new List<string>();
-                for (var j = 0; j < nCols; j++) newRow.Add(Console.ReadLine());
+                for (int j = 0; j < nColumns; j++)
+                {
+                    newRow.Add(Console.ReadLine());
+                }
                 inputFromSender.Add(newRow);
             }
-
             return inputFromSender;
         }
         public virtual int ReadNumberOfRows()
         {
-            int nRows=0;  
-            string temp=Console.ReadLine();
-            if(temp!=null){
+            int nRows = 0;
+            string temp = Console.ReadLine();
+            if (string.IsNullOrEmpty(temp))
+            {
                 nRows = int.Parse(temp);
             }
-            
             return nRows;
         }
         public virtual int ReadNumberOfColumns()
         {
-            int nCols=0;
-            string temp=Console.ReadLine();
-            if(temp!=null)
-                nCols = int.Parse(temp);
-            return nCols;
+            int nColumns = 0;
+            string temp = Console.ReadLine();
+            if (string.IsNullOrEmpty(temp))
+            {
+                nColumns = int.Parse(temp);
+            }
+            return nColumns;
         }
 
-        public void InputExceptionHandler( IEnumerable<IEnumerable<string>> input)
-        {
-            if (!input.Any()) throw new InvalidDataException();
-        }
     }
 }
