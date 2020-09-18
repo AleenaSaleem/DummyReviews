8efa5cd147443dfdad5855d2bf59345c47359496
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-18 13:11:02 +0530

    Update ControllerTests.cs

--- SenderTests/ControllerTests.cs
+++ SenderTests/ControllerTests.cs
@@ -8,14 +8,11 @@ namespace SenderTests
     public class ControllerTests
     {
         const string filepath = @"D:\a\DummyReviews\DummyReviews\SenderTests\TestSample.csv";
-        public CsvInput csvInput;
-        public ConsoleOutput consoleOutput;
-       
         [Fact]
         public static void TestExpectingAnObjectOfCsvInputTypeToBeAssignedToControllersInputInterface()
         {
-            csvInput  = new CsvInput(filepath);
-            consoleOutput= = new ConsoleOutput();
+            CsvInput csvInput  = new CsvInput(filepath);
+            ConsoleOutput consoleOutput= = new ConsoleOutput();
             Controller controller = new Controller(csvInput, consoleOutput);
             var type = controller.InputInterface.GetType();
             Debug.Assert(type == csvInput.GetType());
@@ -24,8 +21,8 @@ namespace SenderTests
         [Fact]
         public void TestExpectingAppropriateReadInputMethodToBeCalledWhenCalled()
         {
-            csvInput  = new CsvInput(filepath);
-            consoleOutput= = new ConsoleOutput();
+            CsvInput csvInput  = new CsvInput(filepath);
+            ConsoleOutput consoleOutput= = new ConsoleOutput();
             Controller controller = new Controller(csvInput, consoleOutput);
             public List<List<string>> parsedInput = (List<List<string>>) controller.ReadInput();
             Assert.Equal("sampledata", parsedInput[0][0]);
@@ -34,8 +31,8 @@ namespace SenderTests
         [Fact]
         public void TestExpectingAppropriateWriteOutputMethodToBeCalledWhenCalledWithTwoDimensionalIEnumerable()
         {
-            csvInput  = new CsvInput(filepath);
-            consoleOutput= = new ConsoleOutput();
+            CsvInput csvInput  = new CsvInput(filepath);
+            ConsoleOutput consoleOutput= = new ConsoleOutput();
             Controller controller = new Controller(csvInput, consoleOutput);
             public List<List<string>> parsedInput = (List<List<string>>) controller.ReadInput();
             controller.WriteOutput(parsedInput);
