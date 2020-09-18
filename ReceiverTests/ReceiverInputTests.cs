c293fd71a1d3fdb4c43e63fc554fdc12c7cbd474
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-18 19:08:49 +0530

    Update ReceiverInputTests.cs

--- ReceiverTests/ReceiverInputTests.cs
+++ ReceiverTests/ReceiverInputTests.cs
@@ -1,43 +1,66 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+using System.Collections.Generic;
 using Receiver;
+using Moq;
 using Xunit;
 
 namespace ReceiverTests
 {
-    public class MockConsoleInput : IReceiverInput
+    
+   /* public class MockConsoleInput : IReceiverInput
     {
+        
         public IEnumerable<IEnumerable<string>> ReadInput()
         {
             int nRows, nColumns;
             nRows = 3;
             nColumns = 3;
             var InputFromSender = new List<List<string>>();
-            for (var i = 0; i < nRows; i++)
+            for (int i = 0; i < nRows; i++)
             {
                 var new_row = new List<string>();
-                for (var j = 0; j < nColumns; j++) new_row.Add("sample" + i + j);
+                for (int j = 0; j < nColumns; j++)
+                {
+                    new_row.Add("sample" + i.ToString() + j.ToString());
+                    
+                }
                 InputFromSender.Add(new_row);
             }
-
             return InputFromSender;
         }
-
         public void InputExceptionHandler(IEnumerable<IEnumerable<string>> Input)
         {
-            if (!Input.Any()) throw new InvalidDataException();
+            if (!Input.Any())
+            {
+                throw new InvalidDataException();
+            }
         }
-    }
-
-    public class ReceiverInputTests
-    {
-        [Fact]
-        public void TestExpectingAnIEnumerableToBeReturnedWhenCalledWithNumRowsAndNumColsAndData()
+    }*/
+        
+        public class ReceiverInputTests
         {
-            var mockObject = new MockConsoleInput();
-            var ActualTestOutput = (List<List<string>>) mockObject.ReadInput();
-            Assert.Equal("sample00", ActualTestOutput[0][0]);
+            private IReceiverInput GetMockConsoleInput()
+            {
+                Mock<ConsoleInput> mockObject = new Mock<ConsoleInput>();
+                var twoDimList = new List<List<string>>();
+                List<string> temp = new List<string>() { "sample1", "sample2" };
+                twoDimList.Add(temp);
+                temp.Add("sample2");
+                twoDimList.Add(temp);
+                mockObject.Setup(rep => rep.ReadInput()).Returns((twoDimList));
+                mockObject.Setup(rep => rep.ReadNumberOfRows()).Returns(twoDimList.Count);
+                mockObject.Setup(rep => rep.ReadNumberOfColumns()).Returns(1);
+                return mockObject.Object;
+            }
+        [Fact]
+            public void TestExpectingAnIEnumerableToBeReturnedWhenCalledWithNumRowsAndNumColsAndData()
+            {
+                
+                ConsoleInput mockObject = (ConsoleInput)this.GetMockConsoleInput();
+                var ActualTestOutput = (List<List<string>>)mockObject.ReadInput();
+                Assert.Equal(2, mockObject.ReadNumberOfRows());
+                Assert.Equal(1, mockObject.ReadNumberOfColumns());
+                Assert.Equal("sample1",ActualTestOutput[0][0]);
+            }
+
         }
-    }
 }
