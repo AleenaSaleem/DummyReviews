78422e17db2c86405291b2d118a1b80ba6e51c72
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-18 12:52:17 +0530

    Update ControllerTests.cs

--- SenderTests/ControllerTests.cs
+++ SenderTests/ControllerTests.cs
@@ -10,11 +10,11 @@ namespace SenderTests
         const string filepath = @"D:\a\DummyReviews\DummyReviews\SenderTests\TestSample.csv";
         public CsvInput csvInput = new CsvInput(filepath);
         public ConsoleOutput consoleOutput = new ConsoleOutput();
-        public Controller controller = new Controller(csvInput, consoleOutput);
-        public List<List<string>> parsedInput = (List<List<string>>) controller.ReadInput();
+       
         [Fact]
         public static void TestExpectingAnObjectOfCsvInputTypeToBeAssignedToControllersInputInterface()
         {
+            Controller controller = new Controller(csvInput, consoleOutput);
             var type = controller.InputInterface.GetType();
             Debug.Assert(type == csvInput.GetType());
         }
@@ -22,13 +22,16 @@ namespace SenderTests
         [Fact]
         public void TestExpectingAppropriateReadInputMethodToBeCalledWhenCalled()
         {
-            
+            Controller controller = new Controller(csvInput, consoleOutput);
+            public List<List<string>> parsedInput = (List<List<string>>) controller.ReadInput();
             Assert.Equal("sampledata", parsedInput[0][0]);
         }
 
         [Fact]
         public void TestExpectingAppropriateWriteOutputMethodToBeCalledWhenCalledWithTwoDimensionalIEnumerable()
         {
+            Controller controller = new Controller(csvInput, consoleOutput);
+            public List<List<string>> parsedInput = (List<List<string>>) controller.ReadInput();
             controller.WriteOutput(parsedInput);
             Assert.Equal("sampledata", consoleOutput.outputData[0]);
         }
