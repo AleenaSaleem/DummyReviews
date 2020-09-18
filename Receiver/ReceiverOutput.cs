167d8827c9820c061e6509acd0037b385f56dddb
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-18 23:09:50 +0530

    Update ReceiverOutput.cs

--- Receiver/ReceiverOutput.cs
+++ Receiver/ReceiverOutput.cs
@@ -1,35 +1,36 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Xml;
 
 namespace Receiver
 {
     public interface IReceiverOutput
     {
-        void WriteOutput(IDictionary<string, int> wordFrequency);
+        void WriteOutput(IDictionary<string, int> WordFrequency);
     }
-
-    public class CSVOutput : IReceiverOutput
+    public class CsvOutput:IReceiverOutput
     {
-        private readonly string filepath;
-
-        public CSVOutput(string filepath)
+        public bool outputStatus;
+        readonly string filepath;
+        public List<string> fileOutput = new List<string>();
+        public CsvOutput(string filepath)
         {
             this.filepath = filepath;
+            outputStatus = false;
         }
-
-        public void WriteOutput(IDictionary<string, int> wordFrequency)
+        public void WriteOutput(IDictionary<string, int> WordFrequency)
         {
             var csv = new StringBuilder();
-            foreach (var line in wordFrequency)
+            foreach(var line in WordFrequency)
             {
                 var newLine = string.Format("{0},{1}", line.Key, line.Value);
-                Console.WriteLine(newLine);
                 csv.AppendLine(newLine);
+                fileOutput.Add(newLine);
             }
-
+           
             File.WriteAllText(filepath, csv.ToString());
+            outputStatus = true;
         }
     }
 }
