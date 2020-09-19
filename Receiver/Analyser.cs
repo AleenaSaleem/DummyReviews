fff8b75a960d8df718713e36242832081eb29b38
Azimcharoliya <39900677+Azimcharoliya@users.noreply.github.com>
2020-09-19 10:46:57 +0530

    Update Analyser.cs

--- Receiver/Analyser.cs
+++ Receiver/Analyser.cs
@@ -2,42 +2,48 @@ using System.Collections.Generic;
 
 namespace Receiver
 {
-    public class Analyser
+    public class Analyzer
     {
+
         public static IEnumerable<string> GetSeparatedWordsBySpaceFromARow(IEnumerable<string> row)
         {
             var separatedRow = new List<string>();
             foreach (var item in row)
             {
                 var words = item.Split(' ');
-                foreach (var word in words) separatedRow.Add(word);
+                separatedRow.AddRange(words);
             }
-
             return separatedRow;
         }
 
-        public static IDictionary<string, int> AddWordCountInDictionary(IDictionary<string, int> wordfrequency,
-            IEnumerable<string> wordsInRow)
+        public static IDictionary<string, int> AddWordCountInDictionary(IDictionary<string, int> wordfrequency, IEnumerable<string> wordsInRow)
         {
             foreach (var word in wordsInRow)
+            {
                 if (!wordfrequency.ContainsKey(word))
+                {
                     wordfrequency.Add(word, 1);
+                }
                 else
+                {
                     wordfrequency[word]++;
+                }
+            }
             return wordfrequency;
         }
 
         public IDictionary<string, int> CountWordFrequency(IEnumerable<IEnumerable<string>> data)
         {
-            IDictionary<string, int> wordfrequency = new Dictionary<string, int>();
+            IDictionary<string, int> wordFrequency = new Dictionary<string, int>();
 
-            foreach (List<string> row in data)
+            foreach (var row in data)
             {
                 var wordsInARow = GetSeparatedWordsBySpaceFromARow(row);
-                wordfrequency = AddWordCountInDictionary(wordfrequency, wordsInARow);
+                wordFrequency = AddWordCountInDictionary(wordFrequency, wordsInARow);
             }
+            return wordFrequency;
 
-            return wordfrequency;
         }
     }
+
 }
